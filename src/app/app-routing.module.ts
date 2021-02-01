ec3f872d45a1949eb7efb22eeadeb3417011c1b9
Oukirche <abdessamade.oukirche@gmail.com>
2021-02-01 21:40:25 +0100

    init project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {StudentsListComponent} from "./students-list/students-list.component";
+import {StudentCardComponent} from "./student-card/student-card.component";
+import {StudentProfileComponent} from "./student-profile/student-profile.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: "", redirectTo: "student-list", pathMatch: "full" },
+    { path: "student-list", component: StudentProfileComponent }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { StudentCardComponent } from './student-card/student-card.component';
+import { StudentsListComponent } from './students-list/students-list.component';
+import { StudentProfileComponent } from './student-profile/student-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    StudentCardComponent,
+    StudentsListComponent,
+    StudentProfileComponent
   ],
   imports: [
     BrowserModule,
